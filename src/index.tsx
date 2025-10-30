3a2e45c2f4efea8bb8eb9780e47a17afbe6e1896
Benja Gilardini <benjag@Benjas-MacBook-Air.local>
2025-10-30 19:10:30 -0300

    setting context

--- src/index.tsx
+++ src/index.tsx
@@ -3,16 +3,20 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { PhrasesProvider } from "./context/PhrasesContext"; 
 
 const container = document.getElementById("root");
 if (!container) {
   throw new Error("No se encontró el elemento #root");
 }
+
 const root = ReactDOM.createRoot(container);
 
 root.render(
   <React.StrictMode>
-    <App />
+    <PhrasesProvider>
+      <App />
+    </PhrasesProvider>
   </React.StrictMode>
 );
 
